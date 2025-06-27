e0765a11e0a4f7d62ff411c0b81d5da3361ca11e
ruziba3vich <rruzibayevich@gmail.com>
2025-06-27 14:52:00 +0500

    fix java_service: fix reflection

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"log"
 	"net"
+	"time"
 
 	"github.com/ruziba3vich/java_service/genprotos/genprotos/compiler_service"
 	"github.com/ruziba3vich/java_service/internal/service"
 	"github.com/ruziba3vich/java_service/pkg/config"
 	logger "github.com/ruziba3vich/prodonik_lgger"
-
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -21,17 +22,34 @@ func NewNetListener(lc fx.Lifecycle, cfg *config.Config) (net.Listener, error) {
 		return nil, err
 	}
 
+	log.Printf("gRPC server will listen on port: %s", cfg.AppPort)
+
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return lis.Close()
 		},
 	})
-
 	return lis, nil
 }
 
 func NewLogger(cfg *config.Config) (*logger.Logger, error) {
 	return logger.NewLogger(cfg.LogPath)
+}
+
+func NewGRPCServer() *grpc.Server {
+	return grpc.NewServer(
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle:     15 * time.Second,
+			MaxConnectionAge:      30 * time.Second,
+			MaxConnectionAgeGrace: 5 * time.Second,
+			Time:                  5 * time.Second,
+			Timeout:               1 * time.Second,
+		}),
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			MinTime:             5 * time.Second,
+			PermitWithoutStream: true,
+		}),
+	)
 }
 
 func main() {
@@ -40,16 +58,16 @@ func main() {
 			config.NewConfig,
 			NewNetListener,
 			NewLogger,
+			NewGRPCServer,
 			service.NewJavaExecutorServer,
 		),
-		fx.Invoke(func(lc fx.Lifecycle, lis net.Listener, server *service.JavaExecutorServer) {
-			s := grpc.NewServer()
+		fx.Invoke(func(lc fx.Lifecycle, lis net.Listener, s *grpc.Server, server *service.JavaExecutorServer, cfg *config.Config) {
 			compiler_service.RegisterCodeExecutorServer(s, server)
 			reflection.Register(s)
 
 			lc.Append(fx.Hook{
 				OnStart: func(ctx context.Context) error {
-					log.Println("gRPC server starting . . .")
+					log.Printf("gRPC server starting on port %s...", cfg.AppPort)
 					go func() {
 						if err := s.Serve(lis); err != nil {
 							log.Printf("gRPC server failed: %v", err)
